e0d31070014268e767244fb741fb776f74f91ec2
Ajaydip Singh <singh@minerva.kgi.edu>
2021-03-13 09:34:22 +0300

    :sparkles: feat: add tweeting functionality

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class TweetViewController: UIViewController {
 
+    @IBOutlet weak var tweetTextView: UITextView!
+    
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func tweet(_ sender: Any) {
+        
+        if (!tweetTextView.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (Error) in
+                print("Error posting tweet \(Error)")
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
     }
     
     override func viewDidLoad() {
